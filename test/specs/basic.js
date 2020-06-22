5f04e7944ee42d1946389fd1f9ef6ef48a39f2a0
Vu Huy Khanh <k001534@cb.local>
2020-06-22 16:23:38 +0700

    24

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -2,7 +2,7 @@ describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io')
         const title = browser.getTitle()
-        console.log("1")
+        console.log("11")
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
     })
 })
